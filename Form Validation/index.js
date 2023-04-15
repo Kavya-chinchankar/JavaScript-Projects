fbd04b7a8ecf1a353ccf7ad2a7392313038499ab
Kavya Krishna chinchankar <kavyachinchankar@gmail.com>
2023-04-15 23:10:58 +0530

    Add files via upload

---
+++ Form Validation/index.js
@@ -0,0 +1,76 @@
+function validate()
+{
+    var username = document.getElementById('username').value
+    var email = document.getElementById('email').value
+    var password = document.getElementById('pass').value
+    var cpassword = document.getElementById('cpassword').value
+
+    checkusername(username)
+    checkemail(email)
+    checkpass(password)
+    checkcpassword(password , cpassword)
+
+}
+
+function checkusername(username)
+{
+     if(username.length > 7)
+     {
+        document.getElementById('username').classList.add('success')
+        document.getElementById('username').classList.replace( 'error' , 'success')
+        document.getElementById('username-error').innerHTML=''
+     }
+     else
+     {
+         document.getElementById('username').classList.add('error')
+         document.getElementById('username-error').innerText='username must be 8 letters long'
+     }
+} 
+
+function checkemail(email)
+{
+     if(email.length>8 && email.includes('@gmail.com'))
+     {
+        document.getElementById('email').classList.add('email')
+        document.getElementById('email').classList.replace( 'error' , 'success')
+        document.getElementById('email-error').innerHTML=''
+     }
+     else
+     {
+         document.getElementById('email').classList.add('error')
+         document.getElementById('email-error').innerText='email must contain @gmail.com , 8 letters long'
+     }
+} 
+
+function checkpass(password)
+{
+     if(password.length > 7 && password.includes('.'))
+     {
+        document.getElementById('pass').classList.add('success')
+        document.getElementById('pass').classList.replace( 'error' , 'success')
+        document.getElementById('pass-error').innerHTML=''
+     }
+     else
+     {
+         document.getElementById('pass').classList.add('error')
+         document.getElementById('pass-error').innerText='password must be 8 letters long and sholud include . oprartors'
+     }
+}
+
+function checkcpassword(password , cpassword)
+{
+     if(password==cpassword && password!='')
+     {
+        document.getElementById('cpassword').classList.add('success')
+        document.getElementById('cpassword').classList.replace( 'error' , 'success')
+        document.getElementById('cpassword-error').innerHTML=''
+     }
+     else
+     {
+         document.getElementById('cpassword').classList.add('error')
+         document.getElementById('cpassword-error').innerText='password not matched'
+     }
+}
+
+
+  
